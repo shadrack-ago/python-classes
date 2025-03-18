2b72375e58c9f29cb838bba4e1174ed000a260a8
Shadrack <shadrackmeshack7@gmail.com>
2025-03-19 00:32:37 +0300

    updates

--- list.py
+++ list.py
@@ -11,3 +11,13 @@ print(languages[-2])
 print(languages[1:3])
 print(languages[:]) #prints all elements
 
+# my_list[2:5] returns a list with items from index 2 to index 4.
+# my_list[5:] returns a list with items from index 1 to the end.
+# my_list[:] returns all list items
+
+my_list.append(6)
+print("after appending", my_list)
+
+my_list.insert(3,7)
+print("after inserting", my_list)
+# so insert you can insert at any index but indicate the position
