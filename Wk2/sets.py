971fd5133e12067391498cd15d1540470d31c8c1
Shadrack <shadrackmeshack7@gmail.com>
2025-04-15 23:42:32 +0300

    sets

---
+++ Wk2/sets.py
@@ -0,0 +1,22 @@
+students_ids = {0, 2, 3, 4, False, "John"}
+# print(students_ids) 0 and False are the same in python
+
+
+
+# employees_ids = {1,  2, 4, 5,8}
+# print(employees_ids)
+# employees_ids.add(89)
+
+
+
+
+
+
+
+account_names = {"John", "Jane","John", "Alice", "Bob", "Mary"}
+print(account_names)
+# print(account_names.pop())
+
+
+names = ("hello world")
+print(names)
