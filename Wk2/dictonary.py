589342060c7f808aa2093cc03bbc232255124644
Shadrack <shadrackmeshack7@gmail.com>
2025-04-15 23:41:03 +0300

    dictionary

---
+++ Wk2/dictonary.py
@@ -0,0 +1,42 @@
+students = {
+    "name": "Evans",
+    "age": 24,
+    "gender": "male",
+    "is_student": True,
+    "courses": ["Math", "Science", "English"],
+
+    "my_name" : {
+        "first_name": "Evans",
+        "last_name": "Kinyua"
+    }
+}
+
+# students["height"] = 2.56
+
+# print(students)
+# print(students["height"])
+
+
+
+
+# capital_cities = {
+#     "Kenya": ["Nairobi", "Mombasa", "Kisumu"],
+#     "Uganda": "Kampala",
+#     "South Africa": "Johannesburg",
+# }
+
+# print(capital_cities)
+# print(capital_cities["South Africa"])
+
+
+
+
+
+capital_cities = {
+    "kenya": ["Nairobi", "Mombasa", "Kisumu"],
+    "Nigeria": "Abuja",
+    "Uganda": 'Kampala',
+}
+
+print(capital_cities)
+print(capital_cities["Uganda"])
