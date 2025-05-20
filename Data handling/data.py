2129bf9ef3e5cd16701e89a568b23c51aa40b162
Shadrack <shadrackmeshack7@gmail.com>
2025-05-20 13:59:04 +0300

    opening file

---
+++ Data handling/data.py
@@ -0,0 +1,9 @@
+
+
+with open("Data handling/example.txt", "r") as file:
+      data = file.read()
+      print("File content:")
+      print(data)
+
+
+
--- week4/file.py
+++ week4/file.py
@@ -16,4 +16,3 @@ with open('week4/output.txt', 'w') as output_file:
 
 # Step 5: Print a success message
 print("The file 'output.txt' has been created successfully.")
-
